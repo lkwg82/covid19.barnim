9b435517bb7d85a4aa0807d8d85382f9faa1f1ee
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-12-29 16:07:23 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <header class="bg-white shadow">
+      <div class="bg-red px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold leading-tight text-white">
+          Covid19 im Barnim
+        </h1>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer
+      class="fixed inset-x-0 bottom-0 bg-red py-3 px-5 text-white font-bold"
+    >
+      <a
+        href="https://github.com/lkwg82/covid19.barnim"
+        target="https://github.com/lkwg82/covid19.barnim"
+        >Über diese Seite</a
+      >
+    </footer>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<script setup>
+import LinkItem from "@/components/LinkItem.vue";
+</script>
+
+<template>
+  <div class="bg-gray-50">
+    <div class="p-2">
+      <p class="m-auto">
+        Wichtige Links zur Bekämpfung der Covid19 Pandemie im Barnim
+      </p>
+    </div>
+
+    <link-item
+      title="Testen?"
+      link="https://covid19.barnim.de/fileadmin/portal/corona/Karten/Corona-Teststationen-Barnim.pdf"
+      linktext="Liste der Teststationen (PDF)"
+    />
+    <link-item
+      title="Impfen?"
+      link="http://termin.impfen-barnim.de"
+      linktext="Impftermin reservieren"
+    />
+    <link-item
+      title="Weitere Informationen"
+      link="https://covid19.barnim.de/"
+      linktext="covid19.barnim.de"
+    />
+  </div>
+</template>
