6e770dc3825dde935e0142a0e62d7bc7c231c47f
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-12-29 18:58:26 +0100

    wip

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import Home from './views/Home.vue'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-    { path: '/', component: Home, meta: { title: 'Home' } },
+    { path: '/:path(.*)', component: Home, meta: { title: 'Home' } },
     // {
     //     path: '/about',
     //     meta: { title: 'About' },
