fa6b6df7bec7e77465070714476bec23e5ca79da
Lars K.W. Gohlke <lkwg82@gmx.de>
2021-12-29 16:44:43 +0100

    initial commit

---
+++ src/components/LinkItem.vue
@@ -0,0 +1,27 @@
+<script >
+export default {
+  props: ["title", "link", "linktext"],
+};
+</script>
+
+<template>
+  <div class="max-w-screen-xl px-4 py-5 mx-auto sm:px-6 lg:py-16 lg:px-20">
+    <h2
+      class="
+        text-3xl
+        font-extrabold
+        leading-9
+        tracking-tight
+        text-gray-900
+        sm:text-4xl sm:leading-10
+      "
+    >
+      {{ title }}
+    </h2>
+    <div class="pt-2 lg:pt-5">
+      <a class="text-indigo-600 font-bold" :href="link" :target="link">
+        {{ linktext }}
+      </a>
+    </div>
+  </div>
+</template>
